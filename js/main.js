f77557813339d11412efc44674e78c62ed8235f5
pascal-codecat <pascal.zwicker@windowslive.com>
2017-02-16 23:27:58 +0100

    added contact section

--- js/main.js
+++ js/main.js
@@ -14,3 +14,30 @@ $(document).ready(function(){
        $("#navigation").toggleClass("hidden-xs");
    });
 });
+
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.6586018, lng: 9.4317064},
+    zoom: 15
+  });
+}
+
+// marker
+
+function initMap() {
+  var myLatLng = {lat: 47.651483, lng: 9.494621};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
