15d7ee09c966bfde31344247a812f991f53820ae
pascal-codecat <pascal.zwicker@windowslive.com>
2017-02-18 22:02:01 +0100

    fixed width footer header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Navbar burger-icon
 function openNav() {
-    document.getElementById("mySidenav").style.width = "15em";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
